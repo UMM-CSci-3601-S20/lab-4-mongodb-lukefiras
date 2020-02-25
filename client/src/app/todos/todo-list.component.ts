81370581e2bbefe01cd00d29d99e684d28b48155
Luke <burde041@morris.umn.edu>
2020-02-25 14:42:34 -0600

    Added Todo-List component
    Added to the todo list component in the client.

---
+++ client/src/app/todos/todo-list.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-todo-list-component',
+  templateUrl: 'todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+  providers: []
+})
+
+export class TodoListComponent implements OnInit, OnDestroy  {
+  // These are public so that tests can reference them (.spec.ts)
+  public serverFilteredTodos: Todo[];
+  public filteredTodos: Todo[];
+
+  public todoOwner: string;
+  public todoStatus: '';
+  public todoCategory: string;
+  public todoBody: string;
+  public todoLimit: number;
+  getTodosSub: Subscription;
+
+
+  // Inject the TodoService into this component.
+  // That's what happens in the following constructor.
+  //
+  // We can call upon the service for interacting
+  // with the server.
+
+  constructor(private todoService: TodoService) {
+
+  }
+
+  getTodosFromServer() {
+    this.todoService.getTodos({
+      status: this.todoStatus,
+      limit: this.todoLimit
+    }).subscribe(returnedTodos => {
+      this.serverFilteredTodos = returnedTodos;
+      this.updateFilter();
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  public updateFilter() {
+    this.filteredTodos = this.todoService.filterTodos(
+      this.serverFilteredTodos, { owner: this.todoOwner, body: this.todoBody, category: this.todoCategory });
+  }
+
+  /**
+   * Starts an asynchronous operation to update the todos list
+   *
+   */
+  ngOnInit(): void {
+    this.getTodosFromServer();
+  }
+
+  ngOnDestroy(): void {
+    this.unsub();
+  }
+
+  unsub(): void {
+    if (this.getTodosSub) {
+      this.getTodosSub.unsubscribe();
+    }
+  }
+}
