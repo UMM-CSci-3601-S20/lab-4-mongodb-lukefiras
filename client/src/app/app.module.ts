e3b57ecdacc9a15cd5fc0ac06a32e77d07e8139b
Luke <burde041@morris.umn.edu>
2020-02-25 14:43:50 -0600

    Added broken routing to Todo-List
    Added routing that does not work yet due to reference to change that does not exist.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,6 +32,8 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { UserCardComponent } from './users/user-card.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { AddUserComponent } from './users/add-user.component';
+import { TodoListComponent } from './todos/todo-list.component';
+import { TodoService } from './todos/todo.service';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
@@ -60,6 +62,7 @@ const MATERIAL_MODULES: any[] = [
     UserCardComponent,
     UserProfileComponent,
     AddUserComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
@@ -73,7 +76,8 @@ const MATERIAL_MODULES: any[] = [
     LayoutModule,
   ],
   providers: [
-    UserService
+    UserService,
+    TodoService
   ],
   bootstrap: [AppComponent]
 })
