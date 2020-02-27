3f84da4e58425778ee0cabf4173812b53943ef9f
Luke <burde041@morris.umn.edu>
2020-02-27 13:18:26 -0600

    Add Todos Endpoint to Server

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -9,7 +9,7 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 import io.javalin.Javalin;
-
+import umm3601.todo.TodoController;
 import umm3601.user.UserController;
 
 public class Server {
@@ -38,6 +38,7 @@ public class Server {
 
     // Initialize dependencies
     UserController userController = new UserController(database);
+    TodoController todoController = new TodoController(database);
     //UserRequestHandler userRequestHandler = new UserRequestHandler(userController);
 
     Javalin server = Javalin.create().start(4567);
@@ -55,6 +56,9 @@ public class Server {
 
     // List users, filtered using query parameters
     server.get("api/users", userController::getUsers);
+
+    // List todos
+    server.get("api/todos", todoController::getTodos);
 
     // Add new user
     server.post("api/users/new", userController::addNewUser);
