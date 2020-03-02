629ca9dfe7acd1ff1fdc6395b4f13c603e6c4128
Firas Naber <naber024@morris.umn.edu>
2020-03-01 23:51:07 -0600

    Add todo list po.ts file
    Added the todo-list.po.ts file. for issue #10
    
    Also, fixed some comments in the todo list e2e file.

--- client/e2e/src/todo-list.e2e-spec.ts
+++ client/e2e/src/todo-list.e2e-spec.ts
@@ -17,61 +17,61 @@ describe('Todo list', () => {
   it('Should type something in the owner filter and check that it returned correct elements', async () => {
     await page.typeInput('todo-owner-input', 'Barry');
 
-    // All of the todo cards should have the name we are filtering by
-    page.getTodos().each(e => {
+    // All of the todo list items should have the name we are filtering by
+    page.getTodoListItems().each(e => {
       expect(e.element(by.className('todo-list-owner')).getText()).toEqual('Barry');
     });
   });
 
-  it('Should type something in the company filter and check that it returned correct elements', async () => {
-    await page.typeInput('todo-company-input', 'OHMNET');
+  it('Should type something in the body filter and check that it returned correct elements', async () => {
+    await page.typeInput('todo-body-input', 'OHMNET');
 
-    // All of the todo cards should have the company we are filtering by
-    page.getTodoCards().each(e => {
-      expect(e.element(by.className('todo-card-company')).getText()).toEqual('OHMNET');
+    // All of the todo list items should have the body we are filtering by
+    page.getTodoListItems().each(e => {
+      expect(e.element(by.className('todo-list-body')).getText()).toEqual('OHMNET');
     });
   });
 
-  it('Should type something partial in the company filter and check that it returned correct elements', async () => {
-    await page.typeInput('todo-company-input', 'ti');
+  it('Should type something partial in the body filter and check that it returned correct elements', async () => {
+    await page.typeInput('todo-body-input', 'ti');
 
-    // Go through each of the cards that are being shown and get the companies
-    const companies = await page.getTodoCards().map(e => e.element(by.className('todo-card-company')).getText());
+    // Go through each of the list items that are being shown and get the companies
+    const bodies = await page.getTodoListItems().map(e => e.element(by.className('todo-list-body')).getText());
 
     // We should see these companies
-    expect(companies).toContain('MOMENTIA');
-    expect(companies).toContain('KINETICUT');
+    expect(bodies).toContain('MOMENTIA');
+    expect(bodies).toContain('KINETICUT');
 
     // We shouldn't see these companies
-    expect(companies).not.toContain('DATAGENE');
-    expect(companies).not.toContain('OHMNET');
+    expect(bodies).not.toContain('DATAGENE');
+    expect(bodies).not.toContain('OHMNET');
   });
 
-  it('Should type something in the age filter and check that it returned correct elements', async () => {
-    await page.typeInput('todo-age-input', '27');
+  it('Should type something in the status filter and check that it returned correct elements', async () => {
+    await page.typeInput('todo-age-input', 'true');
 
-    // Go through each of the cards that are being shown and get the names
-    const names = await page.getTodoCards().map(e => e.element(by.className('todo-card-name')).getText());
+    // Go through each of the list items that are being shown and get the names
+    const owners = await page.getTodoListItems().map(e => e.element(by.className('todo-list-owner')).getText());
 
-    // We should see these todos whose age is 27
-    expect(names).toContain('Stokes Clayton');
-    expect(names).toContain('Bolton Monroe');
-    expect(names).toContain('Merrill Parker');
+    // We should see these todos whose status is true
+    expect(owners).toContain('Stokes Clayton');
+    expect(owners).toContain('Bolton Monroe');
+    expect(owners).toContain('Merrill Parker');
 
     // We shouldn't see these todos
-    expect(names).not.toContain('Connie Stewart');
-    expect(names).not.toContain('Lynn Ferguson');
+    expect(owners).not.toContain('Connie Stewart');
+    expect(owners).not.toContain('Lynn Ferguson');
   });
 
   it('Should change the view', async () => {
     await page.changeView('list');
 
-    expect(page.getTodoCards().count()).toEqual(0); // There should be no cards
+    expect(page.getTodoListItems().count()).toEqual(0); // There should be no list items
     expect(page.getTodoListItems().count()).toBeGreaterThan(0); // There should be list items
   });
 
-  it('Should select a role, switch the view, and check that it returned correct elements', async () => {
-    await page.selectMatSelectValue('todo-role-select', 'viewer');
+  it('Should select a category, switch the view, and check that it returned correct elements', async () => {
+    await page.selectMatSelectValue('todo-category-select', 'math');
     await page.changeView('list');
 
     expect(page.getTodoListItems().count()).toBeGreaterThan(0);
@@ -85,20 +85,20 @@ describe('Todo list', () => {
   });
 
   it('Should click view profile on a todo and go to the right URL', async () => {
-    const firstTodoName = await page.getTodoCards().first().element(by.className('todo-card-name')).getText();
-    const firstTodoCompany = await page.getTodoCards().first().element(by.className('todo-card-company')).getText();
-    await page.clickViewProfile(page.getTodoCards().first());
+    const firstTodoOwner = await page.getTodoListItems().first().element(by.className('todo-owner-input')).getText();
+    const firstTodoBody = await page.getTodoListItems().first().element(by.className('todo-body-input')).getText();
+    await page.clickViewProfile(page.getTodoListItems().first());
 
     // Wait until the URL contains 'todos/' (note the ending slash)
     await browser.wait(EC.urlContains('todos/'), 10000);
 
-    // When the view profile button on the first todo card is clicked, the URL should have a valid mongo ID
+    // When the view profile button on the first todo list item is clicked, the URL should have a valid mongo ID
     const url = await page.getUrl();
     expect(RegExp('.*\/todos\/[0-9a-fA-F]{24}$', 'i').test(url)).toBe(true);
 
-    // On this profile page we were sent to, the name and company should be correct
-    expect(element(by.className('todo-card-name')).getText()).toEqual(firstTodoName);
-    expect(element(by.className('todo-card-company')).getText()).toEqual(firstTodoCompany);
+    // On this profile page we were sent to, the owner and body should be correct
+    expect(element(by.className('todo-owner-input')).getText()).toEqual(firstTodoOwner);
+    expect(element(by.className('todo-body-input')).getText()).toEqual(firstTodoBody);
   });
 
   it('Should click add todo and go to the right URL', async () => {
@@ -107,7 +107,7 @@ describe('Todo list', () => {
     // Wait until the URL contains 'todos/new'
     await browser.wait(EC.urlContains('todos/new'), 10000);
 
-    // When the view profile button on the first todo card is clicked, we should be sent to the right URL
+    // When the view profile button on the first todo list item is clicked, we should be sent to the right URL
     const url = await page.getUrl();
     expect(url.endsWith('/todos/new')).toBe(true);
 
